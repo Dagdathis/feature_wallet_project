7e892234ab21df2040e92b806921d2b3bdd0f48f
Dagdathis <97541438+Dagdathis@users.noreply.github.com>
2022-06-06 18:06:43 +0300

    Add files via upload

--- diagram_py.py
+++ diagram_py.py
@@ -7,38 +7,50 @@ y = []
 with open('data.csv','r') as sales_csv:
     plots = csv.reader(sales_csv, delimiter=';')
     for row in plots:
-        x.append(row[1])        #Колонка с типом 
-        y.append(row[2])        #Колонка с суммой
+        x.append(row[2])        #Колонка с типом 
+        y.append(row[3])        #Колонка с суммой
 
-                        #соединим два списка специальной функцией zip
-a = zip(x,y)            #a теперь по типу [(2, 1000), (3, 400).....]
+#соединил два списка функцией zip                        
+a = zip(x,y)                            #a теперь по типу [(2, 1000), (3, 400).....]
 
-                                        #отсортируем, взяв первый элемент каждого списка как ключ
+#сортируем, взяв первый элемент каждого списка как ключ
 aas = sorted(a, key=lambda tup: tup[0])     #aas = [(1, 'b'), (2, 'c'), (3, 'a')]
-                        #и последний шаг - извлечем
+
+#извлечем
 x1 = [a[0] for a in aas]
 y1 = [a[1] for a in aas]
 
-                        #Цикл для суммирования трат по типу
+# перевод из string в int
+x2 = []  
+for item in x1:
+    x2.append(int(item))
+print(x2)
 
-#ras =[]                #Пробовал складывать сумму по коду,
-#i=0                    #Выдаёт ошибку TypeError: unsupported operand type(s) for +=: 'int' and 'str'  
-#j=0
-#for i on x1:                       
+# перевод из string в float
+y2 = []
+for item in y1: 
+    y2.append(float(item))
+print(y2)
+
+#Цикл для суммирования трат по типу
+#ras =[]                             
+#for i in range(len(x2)):
 #    n=0
 #    suma=0
-#    for j on x1: 
-#        if i==j:
-#            n += y1[i]            
-#        else:
-#            n += y1[i]
-#            suma=n
-#            ras.append(suma) # = [15, 25, 25, 30, 5] 
+#    for j in range(i + 1, len(x2)):
+#        if x2[i] != x2[j]:
+#            n += y2[i]
+#            suma+=n
+#            ras.append(suma) # тестовые значения = [3700, 1700, 40, 9000] 
 #            n=0
-ras = [15, 25, 25, 30, 5]    #Диаграмма работает
-print(ras)
+#            break
+#        else:   
+#            n += y2[j]
 
-labels = ['Type 1', 'Type 2', 'Type 3', 'Type 4', 'Type 5'] 
+#print(ras)      #проверка массива
+ras =[3700, 1700, 40, 9000]
+#Создание диаграмы
+labels = ['Type 2', 'Type 3', 'Type 4', 'Type 5'] 
 
 fig, ax = plt.subplots() 
 ax.pie(ras, labels = labels) 
